b87b34865d1dc7e09fb8eff15de1b483c8068ae9
Jemima <jemimanyaguthiiwambugu@gmail.com>
2024-04-05 13:51:58 +0300

    Implemented Login and SignUp

--- app/src/main/java/com/example/authentication/screens/LoginScreen.kt
+++ app/src/main/java/com/example/authentication/screens/LoginScreen.kt
@@ -1,7 +1,9 @@
 package com.example.authentication.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,6 +16,8 @@ import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -24,6 +28,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
@@ -34,6 +39,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -45,27 +51,33 @@ fun LoginPage(navController: NavHostController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(15.dp, 56.dp,15.dp,15.dp)
+            .padding(15.dp, 56.dp, 15.dp, 15.dp)
     )
     {
         val username = remember { mutableStateOf("") }
-        var password = remember { mutableStateOf("")}
+        var password = remember { mutableStateOf("") }
         var isPasswordVisible by remember { mutableStateOf(false) }
-
-
-        Text(text = "Sign In", style = TextStyle(fontSize = 30.sp, fontFamily = FontFamily.Serif, fontWeight = FontWeight.Black))
+        var rememberMe by remember { mutableStateOf(false) }
+        Text(
+            text = "Sign In",
+            style = TextStyle(
+                fontSize = 30.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Black
+            )
+        )
 
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Username", style = TextStyle(fontSize =15.sp , fontFamily = FontFamily.Serif))
+        Text(text = "Username", style = TextStyle(fontSize = 15.sp, fontFamily = FontFamily.Serif))
         OutlinedTextField(
             label = { Text(text = "Your username") },
             value = username.value,
-            onValueChange = { username.value = it } ,
+            onValueChange = { username.value = it },
             modifier = Modifier.fillMaxWidth()
         )
 
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Password", style = TextStyle(fontSize =15.sp , fontFamily = FontFamily.Serif))
+        Text(text = "Password", style = TextStyle(fontSize = 15.sp, fontFamily = FontFamily.Serif))
         OutlinedTextField(
             value = password.value,
             onValueChange = { password.value = it },
@@ -81,23 +93,50 @@ fun LoginPage(navController: NavHostController) {
                 }
             }
         )
-
         Spacer(modifier = Modifier.height(20.dp))
 
-        ClickableText(
-            text = AnnotatedString("Forgot password?"),
-            onClick = { },
-            style = TextStyle(
-                fontSize = 14.sp,
-                fontFamily = FontFamily.Serif
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = rememberMe,
+                    onCheckedChange = { isChecked ->
+                        rememberMe = isChecked
+
+                    },
+                    colors = CheckboxDefaults.colors(
+                        checkedColor = Color.Magenta
+                    )
+                )
+                Text(
+                    text = "Remember me",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily.Serif
+                    )
+                )
+            }
+            ClickableText(
+                text = AnnotatedString("Forgot password?"),
+                onClick = { },
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily.Serif
+                )
             )
-        )
+        }
         Spacer(modifier = Modifier.height(20.dp))
 
         Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
             Button(
                 onClick = {
-                    navController.navigate(Routes.SignUp.route) },
+                    navController.navigate(Routes.SignUp.route)
+                },
                 shape = RoundedCornerShape(50.dp),
                 modifier = Modifier
                     .fillMaxWidth()
@@ -106,12 +145,38 @@ fun LoginPage(navController: NavHostController) {
                     containerColor = Color.Magenta
                 )
             ) {
-                Text(text = "Sign In",style = TextStyle(fontSize = 20.sp,fontFamily = FontFamily.Serif))
+                Text(
+                    text = "Sign In",
+                    style = TextStyle(fontSize = 20.sp, fontFamily = FontFamily.Serif)
+                )
             }
         }
         Spacer(modifier = Modifier.height(20.dp))
-        Text(text = "Don't have an account?",style = TextStyle(fontSize = 15.sp,fontFamily = FontFamily.Serif, textAlign = TextAlign.Center))
 
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Don't have an account? ",
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    fontFamily = FontFamily.Serif
+                )
+            )
+
+            ClickableText(
+                text = AnnotatedString(text = "Sign up"),
+                onClick = {
+                    navController.navigate(Routes.SignUp.route)
+                },
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    fontFamily = FontFamily.Serif,
+                    color = Color.Magenta,
+                   textDecoration = TextDecoration.Underline
+                )
+            )
+        }
     }
 }
-
--- app/src/main/java/com/example/authentication/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/authentication/screens/SignUpScreen.kt
@@ -1,19 +1,25 @@
 package com.example.authentication.screens
 
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -23,7 +29,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
@@ -31,6 +39,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -49,7 +58,7 @@ fun SignUpPage(navController: NavHostController) {
         val email = remember { mutableStateOf("") }
         var password = remember { mutableStateOf("") }
         var isPasswordVisible by remember { mutableStateOf(false) }
-
+        var rememberMe by remember { mutableStateOf(false) }
 
         Text(text = "Create account", style = TextStyle(fontSize = 30.sp, fontFamily = FontFamily.Serif,fontWeight = FontWeight.Black))
 
@@ -62,7 +71,7 @@ fun SignUpPage(navController: NavHostController) {
             modifier = Modifier.fillMaxWidth()
         )
 
-        Spacer(modifier = Modifier.height(26.dp))
+        Spacer(modifier = Modifier.height(10.dp))
         Text(text = "Email", style = TextStyle(fontSize =15.sp , fontFamily = FontFamily.Serif))
         OutlinedTextField(
             value = email.value,
@@ -70,7 +79,7 @@ fun SignUpPage(navController: NavHostController) {
             label = { Text("Your email") },
             modifier = Modifier.fillMaxWidth()
         )
-        Spacer(modifier = Modifier.height(26.dp))
+        Spacer(modifier = Modifier.height(10.dp))
         Text(text = "Password", style = TextStyle(fontSize =15.sp , fontFamily = FontFamily.Serif))
             OutlinedTextField(
             value = password.value,
@@ -87,36 +96,80 @@ fun SignUpPage(navController: NavHostController) {
                     }
                 }
             )
-        Spacer(modifier = Modifier.height(30.dp))
 
-        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
-            Button(
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        )
+            {
+                Checkbox(
+                    checked = rememberMe,
+                    onCheckedChange = { isChecked ->
+                        rememberMe = isChecked
+                    },
+                    colors = CheckboxDefaults.colors(
+                        checkedColor = Color.Magenta
+                    ),
+                )
+                Text(
+                    text = "I accept the terms and privacy policy",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily.Serif
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+                Button(
+                    onClick = {
+                        if (username.value.isEmpty() || email.value.isEmpty() || password.value.isEmpty()) {
+                            return@Button
+                        }
+                        navController.navigate(Routes.Login.route)
+                    },
+
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Magenta
+                    )
+                ) {
+                    Text(
+                        "Sign Up",
+                        style = TextStyle(fontSize = 20.sp, fontFamily = FontFamily.Serif)
+                    )
+                }
+            }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Already have an account? ",
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    fontFamily = FontFamily.Serif
+                )
+            )
+
+            ClickableText(
+                text = AnnotatedString(text = "Log in"),
                 onClick = {
-                    if (username.value.isEmpty() || email.value.isEmpty() || password.value.isEmpty()) {
-                        return@Button
-                    }
                     navController.navigate(Routes.Login.route)
                 },
-
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.Magenta
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    fontFamily = FontFamily.Serif,
+                    color = Color.Magenta,
+                    textDecoration = TextDecoration.Underline
                 )
-            ) {
-                Text("Sign Up", style = TextStyle(fontSize = 20.sp, fontFamily = FontFamily.Serif))
-            }
+            )
         }
-        Spacer(modifier = Modifier.height(20.dp))
-//        ClickableText(
-//            text = AnnotatedString("Forgot password?"),
-//            onClick = { },
-//            style = TextStyle(
-//                fontSize = 14.sp,
-//                fontFamily = FontFamily.Serif
-//            )
-//        )
     }
     }
+
 
